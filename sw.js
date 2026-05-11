c1ed0092b37461c9fa47b583ff1a540b8605c629
a901112 <94549210+a901112@users.noreply.github.com>
2026-05-11 19:59:37 +0800

    Bump cache for V8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "jp-quiz-v7";
+const CACHE_NAME = "jp-quiz-v8";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
