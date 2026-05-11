10328a732b5810ca494f9868062ee8e72c26788c
a901112 <94549210+a901112@users.noreply.github.com>
2026-05-11 08:37:59 +0800

    Bump service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "jp-quiz-v4";
+const CACHE_NAME = "jp-quiz-v6";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
